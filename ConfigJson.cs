7dffd5796249ff612eccf746d47913dbb047f3ec
MsGraphic <67607434+MsGraphic@users.noreply.github.com>
2023-04-22 18:57:35 +0300

    Add files via upload

---
+++ ConfigJson.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+    public struct ConfigJson
+    {
+        [JsonProperty("token")]
+        public string Token { get; private set; }
+        [JsonProperty("prefix")]
+        public string Prefix { get; private set; }
+    }
+}
